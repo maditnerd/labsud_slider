d8b14aa026db3984958e8d0204a136623c2d39f1
Rémi Sarrailh <remi@usini.eu>
2023-09-23 20:04:11 +0200

    fixed for different url

--- functions.js
+++ functions.js
@@ -5,10 +5,13 @@ if (isNaN(page_nb)) {
 }
 
 base_url = window.location.origin + window.location.pathname;
+url_nohtml = base_url.split("/")
+url_nohtml.pop()
+url_nohtml = url_nohtml.join("/");
 
 function change_page(nb){
     document.body.style.animation = 'fadeOut 1s ease-in-out forwards';
     setTimeout(function(){
-        window.location.href = window.location.origin + nb;
+        window.location.href = url_nohtml + nb;
     }, 1000);
 }
